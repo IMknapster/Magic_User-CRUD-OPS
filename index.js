d16a16001c1c6a5586fcad9c5f17e4bcfda0822c
Naveen Kumar Singh <naveen.singh@magicsw.com>
2022-02-20 17:18:33 +0530

    CRUD USER API

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
+const path = require('path');
+const mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost/userdetails", { useNewUrlParser: true },{UnhandledPromiseRejection:true});
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log("MongoDB runnning as well"))
+
+app.use(express.json())
+
+const usersRouter = require('./routes/users')
+
+app.use('/', usersRouter);
+
+
+app.listen(5000, () => {
+    console.log("Server Started");
+})
